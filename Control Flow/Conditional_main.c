916d77b4c58cb984f35e827d08778df84d674f7c
denish <denishakbari1@gmail.com>
2025-12-18 13:34:50 +0530

    Conditional_Main

---
+++ Control Flow/Conditional_main.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+
+int LeapYear()
+{
+
+    int year;
+
+    printf("please enter year \n :");
+    scanf("%d",&year);
+
+
+    if((year % 400) == 0)
+    {
+      printf("%d is leap year \n", year);
+    }
+    else if((year % 100) == 0)
+    {
+
+        printf("%d is note leap year \n",year);
+
+    }
+    else if((year % 4) == 0)
+    {
+       printf("%d is leap year \n",year);
+    }
+    else 
+    {
+      printf("%d is not leap year \n",year);
+    }
+
+
+
+
+}
+
+int main()
+{
+
+
+
+
+
+    return 0;
+}
