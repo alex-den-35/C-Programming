9824206c11a00a26cf495a07592abeb21e389cdd
denish <denishakbari1@gmail.com>
2025-12-18 12:14:28 +0530

    strat again

---
+++ Control Flow/Equality_Check.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main()
+{
+
+
+
+    int a,b,c;
+
+
+    printf("plase enter first number \n :");
+    scanf("%d",&a);
+    printf("\n");
+    printf("please entr second number \n :");
+    scanf("%d",&b);
+
+
+    if(a==b)
+    {
+         printf("both number are same \n");
+    }else
+    {
+        printf("bother number are different \n");
+    }
+
+    return 0;
+
+}
+
