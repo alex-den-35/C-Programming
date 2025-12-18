86b52e6496e0b67c364c9f8c059c8e546c9ea4d8
denish <denishakbari1@gmail.com>
2025-12-18 13:18:20 +0530

    Positive - Negative

---
+++ Control Flow/Positive_Negative.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+
+
+    int a,b,result;
+    printf("please enter the number \n :");
+    scanf("%d",&a);
+
+
+    if(a>=0)
+    {
+      printf("number is positive \n");
+
+    }else
+    {
+       printf("the number is negative \n");
+    }
+
+
+
+
+    return 0;
+}
